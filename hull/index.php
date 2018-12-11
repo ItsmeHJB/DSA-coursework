3711e76feb6865ecc324b6a43a3006b2e6c07513
m3-welch <morgan2.welch@live.uwe.ac.uk>
2018-12-11 11:20:05 +0000

    Minor tweaks
    Signed-off-by: m3-welch <morgan2.welch@live.uwe.ac.uk>

--- hull/index.php
+++ hull/index.php
@@ -1,8 +1,9 @@
 <html>
 <head>
-    <title>Hull Information</title>
+    <title>Kingston-Upon Hull Information</title>
+    <link rel = "stylesheet" href = "hullstyle.css"/>
 </head>
 <body>
-
+    <span class = "cityname">Kingston-Upon Hull Information</span>
 </body>
 </html>
--- rotterdam/index.php
+++ rotterdam/index.php
@@ -1,8 +1,9 @@
 <html>
 <head>
     <title>Rotterdam</title>
+    <link rel = "stylesheet" href = "rotterdamstyle.css"/>
 </head>
 <body>
-
+    <span class = "cityname">Rotterdam Information</span>
 </body>
 </html>
