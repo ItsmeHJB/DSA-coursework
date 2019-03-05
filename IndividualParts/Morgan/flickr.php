4d6e8213aae4b48946144966895505c2511941a1
m3-welch <morgan2.welch@live.uwe.ac.uk>
2019-03-05 02:00:48 +0000

    Caching works!
    Signed-off-by: m3-welch <Morgan2.Welch@live.uwe.ac.uk>

--- IndividualParts/Morgan/flickr.php
+++ IndividualParts/Morgan/flickr.php
@@ -17,6 +17,8 @@ $response = json_decode($response);
 
 //print_r($response);
 
+$images = array();
+
 echo "<div class = 'flickr'>";
 echo "<div class = 'flickrtitle'>";
 echo "<span class = 'flickrtitle'>Photos of {$name} from <a href = 'https://www.flickr.com'><span class = 'flick'>flick</span><span class = 'r'>r</span></a></span>";
@@ -29,39 +31,66 @@ for($i = 0; $i < 50; $i++){
   $id = $response->photos->photo[$i]->id;
   $secret = $response->photos->photo[$i]->secret;
   $picture_url = "https://farm{$farmid}.staticflickr.com/{$serverid}/{$id}_{$secret}.jpg";
+  array_push($images, $picture_url);
   echo "<img crossorigin='anonymous' id ='flickrImage{$name}{$i}' class = 'flickrimage' src='{$picture_url}'/>";
 }
 echo "</div>";
 echo "</div>";
+
+$images = json_encode($images);
+
 print <<<EOT
 
-<script>
-    images = [];
-    str = "flickrImage";
-    for(i = 0; i < 50; i++){
-        thisID = str.concat("{$name}", i.toString());
-        images.push(document.getElementById(thisID));
-        var imgCanvas = document.createElement("canvas"),
-            imgContext = imgCanvas.getContext("2d");
-        
-        imgCanvas.width = images[i].width;
-        imgCanvas.height = images[i].height;
-        
-        imgContext.drawImage(images[i], 0, 0, images[i].width, images[i].height);
-        
-        var imgAsDataURL = imgCanvas.toDataURL("image/jpeg");
-        console.log("imgAsDataURL = " + imgAsDataURL);
-        
-        try{
-            localStorage.setItem(thisID, imgAsDataURL);
-        }
-        catch(e){
-            console.log("Storage failed: " + e);
-        }
-    }
-    
-    
-</script>
+  <script>
+      images = [];
+      imageURLs = {$images};
+      str = "flickrImage";
+      
+      for(i = 0; i < 50; i++){
+          thisID = str.concat("{$name}", i.toString());
+          images.push(document.getElementById(thisID));
+          console.log(images[i]);
+          var storageFile = localStorage.getItem(thisID) || {},
+            thisIDdate = "dateStored" + thisID,
+            storageFileDate = localStorage.getItem(thisIDdate),
+            date = new Date(),
+            todaysDate = (date.getMonth() + 1).toString() + date.getDate().toString();
+            
+          // console.log("Typeof date: " + typeof storageFileDate);
+          // console.log("storageDate: " + storageFileDate);
+          // console.log("todaysDate: " + todaysDate);
+          
+          if(typeof storageFileDate === "undefined" || storageFileDate < todaysDate){
+              
+
+                  var imgCanvas = document.createElement("canvas"),
+                      imgContext = imgCanvas.getContext("2d");
+                  
+                  imgCanvas.width = images[i].width;
+                  imgCanvas.height = images[i].height;
+                  
+                  imgContext.drawImage(images[i], 0, 0, images[i].width, images[i].height);
+                  
+                  var imgAsDataURL = imgCanvas.toDataURL("image/jpeg");
+                  
+                  try{
+                      localStorage.setItem(thisID, imgAsDataURL);
+                      localStorage.setItem(thisIDdate, todaysDate);
+                  }
+                  catch(e){
+                      console.log("Storage failed: " + e);
+                  }
+              
+              images[i].setAttribute("src", imageURLs[i]);
+              console.log("Saved image: " + (i + 1));
+          }
+          else{
+              images[i].setAttribute("src", storageFile);
+              console.log("Loaded image: " + (i + 1));
+          }
+      }
+      
+  </script>
 EOT;
 
 ?>
