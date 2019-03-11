fb37f9cbbad941647821e65bef2d1792e11ff5ae
m3-welch <morgan2.welch@live.uwe.ac.uk>
2019-03-11 22:17:35 +0000

    updated the database sql file and the group xml
    Signed-off-by: m3-welch <morgan2.welch@live.uwe.ac.uk>

--- Database/db_twincities.sql
+++ Database/db_twincities.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 19, 2018 at 09:49 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.2.12
+-- Host: localhost
+-- Generation Time: Mar 11, 2019 at 11:16 PM
+-- Server version: 5.7.25-0ubuntu0.18.04.2
+-- PHP Version: 7.2.15-0ubuntu0.18.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,10 +25,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_city`
+-- Table structure for table `tb_cities`
 --
 
-CREATE TABLE `tb_city` (
+CREATE TABLE `tb_cities` (
   `woeid_city` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
   `latitude` double NOT NULL,
@@ -43,20 +43,20 @@ CREATE TABLE `tb_city` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_city`
+-- Dumping data for table `tb_cities`
 --
 
-INSERT INTO `tb_city` (`woeid_city`, `name`, `latitude`, `longitude`, `country`, `population`, `currency`, `province`, `area`, `time_zone`, `website`) VALUES
+INSERT INTO `tb_cities` (`woeid_city`, `name`, `latitude`, `longitude`, `country`, `population`, `currency`, `province`, `area`, `time_zone`, `website`) VALUES
 (25211, 'Kingston-Upon Hull', 53.73833038, -0.333332, 'United Kingdom', 319093, 'British Pound Stirling', 'Yorkshire', 71.45, 0, 'http://www.hull.gov.uk/'),
 (733075, 'Rotterdam', 51.933312, 4.489051, 'Netherlands', 1007780, 'Euro', 'South Holland', 325.8, 1, 'https://www.rotterdam.nl');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_photo`
+-- Table structure for table `tb_photos`
 --
 
-CREATE TABLE `tb_photo` (
+CREATE TABLE `tb_photos` (
   `photo_id` int(11) NOT NULL,
   `link` varchar(128) NOT NULL,
   `woeid_city` int(11) NOT NULL,
@@ -65,10 +65,10 @@ CREATE TABLE `tb_photo` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_photo`
+-- Dumping data for table `tb_photos`
 --
 
-INSERT INTO `tb_photo` (`photo_id`, `link`, `woeid_city`, `woeid_poi`, `name`) VALUES
+INSERT INTO `tb_photos` (`photo_id`, `link`, `woeid_city`, `woeid_poi`, `name`) VALUES
 (1, 'https://www.thedeep.co.uk/public/skin/social-fallback.jpg', 25211, 56870962, 'The Deep Aquarium'),
 (2, 'https://i.pinimg.com/originals/ae/35/8a/ae358ada6fec91ea0de8e69fadad5f68.jpg', 25211, 92158782, 'Dinostar'),
 (3, 'https://media-cdn.tripadvisor.com/media/photo-s/0c/8c/b5/e9/welcome-to-burton-constable.jpg', 25211, 14679, 'Burton Constable Hall'),
@@ -92,7 +92,7 @@ INSERT INTO `tb_photo` (`photo_id`, `link`, `woeid_city`, `woeid_poi`, `name`) V
 (21, 'https://media-cdn.tripadvisor.com/media/photo-s/0a/dc/48/1d/dsc-0128-largejpg.jpg', 25211, 56816533, 'Hull City Centre'),
 (22, 'https://www.hull.ac.uk/assets/section/homepage/images/content/campus.x32d640d9.jpg', 25211, 23702074, 'University of Hull'),
 (23, 'https://www.visitengland.com/sites/default/files/Hull-University--Derwent-in-Summer-MSP_7985-copy_0.jpg', 25211, 23702074, 'University of Hull'),
-(24, 'https://www.findamasters.com/bespokePages/featuredlistings/BPID2923/21571.jpg', 25211, 23702074, 'University of Hull'),
+(24, 'https://goo.gl/3AKwtw', 25211, 23702074, 'University of Hull'),
 (25, 'https://media.studentcrowd.net/w426-h284-q70-cfill/830671355502661_university-of-hull.jpg', 25211, 23702074, 'University of Hull'),
 (26, 'https://upload.wikimedia.org/wikipedia/commons/e/e0/Maritiem_Museum_Rotterdam_1_Fotografie_Marco_de_Swart.jpg', 733075, 56816568, 'Maritime Museum Rotterdam'),
 (27, 'https://media-cdn.tripadvisor.com/media/photo-s/01/fc/28/8b/early-am.jpg', 733075, 56816568, 'Maritime Museum Rotterdam'),
@@ -110,23 +110,23 @@ INSERT INTO `tb_photo` (`photo_id`, `link`, `woeid_city`, `woeid_poi`, `name`) V
 (39, 'https://media-cdn.tripadvisor.com/media/photo-s/12/78/e1/37/oceanium-diergaarde-blijdorp.jpg', 733075, 22384441, 'Diergaarde Blijdorp: The Royal Zoo'),
 (40, 'https://pubblestorage.blob.core.windows.net/d9c7ad83/content/2018/7/3064af81-27a1-42e5-bd82-f564e51ab83a.jpg', 733075, 22384441, 'Diergaarde Blijdorp: The Royal Zoo'),
 (41, 'https://tickets.holland.com/wp-content/uploads/2016/09/euromast-12-e1500563789151.jpg', 733075, 729194, 'Euromast'),
-(42, 'https://tickets.holland.com/wp-content/uploads/2016/09/euromast-12-e1500563789151.jpg', 733075, 729194, 'Euromast'),
-(43, 'https://tickets.holland.com/wp-content/uploads/2016/09/euromast-12-e1500563789151.jpg', 733075, 729194, 'Euromast'),
-(44, 'https://tickets.holland.com/wp-content/uploads/2016/09/euromast-12-e1500563789151.jpg', 733075, 729194, 'Euromast'),
-(45, 'https://tickets.holland.com/wp-content/uploads/2016/09/euromast-12-e1500563789151.jpg', 733075, 729194, 'Euromast'),
+(42, 'https://euromast.nl/wp-content/uploads/2017/04/AGM_7464.jpg', 733075, 729194, 'Euromast'),
+(43, 'https://euromast.nl/wp-content/uploads/2017/04/Brasserie-kopie.jpg', 733075, 729194, 'Euromast'),
+(44, 'https://www.cityguiderotterdam.com/fileadmin/_processed_/6/9/csm_Euromast_Rotterdam_watertaxi_slider_1dd832c21d.jpg', 733075, 729194, 'Euromast'),
+(45, 'https://c1.staticflickr.com/5/4479/36610949663_af1215ee41_b.jpg', 733075, 729194, 'Euromast'),
 (46, 'https://www.holland.com/upload_mm/a/6/9/57490_fullimage_32ec68fe4dd0a9691367a3023b3bd92bf3106fb567e91a7ea2b8ccf56e2c0ac7.jpg', 733075, 91412178, 'Erasmus Bridge'),
 (47, 'https://i.ytimg.com/vi/_IzD8ktVq18/maxresdefault.jpg', 733075, 91412178, 'Erasmus Bridge'),
 (48, 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/RotterdamMaasNederland.jpg/770px-RotterdamMaasNederland.jpg', 733075, 91412178, 'Erasmus Bridge'),
 (49, 'https://www.mimoa.eu/images/45665_l.jpg', 733075, 91412178, 'Erasmus Bridge'),
-(50, 'https://goo.gl/iURCWy', 733075, 91412178, 'Erasmus Bridge');
+(50, 'https://www.mediastorehouse.com/p/251/rotterdam-erasmus-bridge-euromast-tower-1655853.jpg', 733075, 91412178, 'Erasmus Bridge');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_poi`
+-- Table structure for table `tb_pois`
 --
 
-CREATE TABLE `tb_poi` (
+CREATE TABLE `tb_pois` (
   `woeid_poi` int(11) NOT NULL,
   `latitude` double DEFAULT NULL,
   `longitude` double DEFAULT NULL,
@@ -141,19 +141,19 @@ CREATE TABLE `tb_poi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tb_poi`
+-- Dumping data for table `tb_pois`
 --
 
-INSERT INTO `tb_poi` (`woeid_poi`, `latitude`, `longitude`, `name`, `capacity`, `cost`, `launch`, `website`, `opening_time`, `closing_time`, `woeid_city`) VALUES
+INSERT INTO `tb_pois` (`woeid_poi`, `latitude`, `longitude`, `name`, `capacity`, `cost`, `launch`, `website`, `opening_time`, `closing_time`, `woeid_city`) VALUES
 (14679, 53.813987, -0.195885, 'Burton Constable Hall', NULL, 11, '1560-01-01', 'https://www.burtonconstable.com/', '11.00', '17.00', 25211),
 (729194, 51.90545, 4.466606, 'Euromast', 1200, 8.76, '1960-01-01', 'https://euromast.nl', '10.00', '22.00', 733075),
-(730730, 51.884508, 4.632675, 'Kinderdijk', NULL, NULL, NULL, 'https://www.kinderdijk.com', NULL, NULL, 733075),
+(730730, 51.884508, 4.632675, 'Kinderdijk', NULL, 8, '1740-01-01', 'https://www.kinderdijk.com', NULL, NULL, 733075),
 (22384441, 51.928132, 4.444112, 'Diergaarde Blijdorp - Royal Zoo', NULL, 21.5, '1857-01-01', 'https://www.diergaardeblijdorp.nl/', '9.00', '17.00', 733075),
 (23702074, 53.773905, -0.367975, 'University of Hull', 18500, NULL, '1954-01-01', 'http://www.hull.ac.uk/', NULL, NULL, 25211),
-(56816533, 53.744428, -0.337735, 'Hull City Centre', NULL, NULL, NULL, NULL, NULL, NULL, 25211),
+(56816533, 53.744428, -0.337735, 'Hull City Centre', NULL, 0, '1193-01-01', 'https://www.visithullandeastyorkshire.com/', NULL, NULL, 25211),
 (56816568, 51.917605, 4.48227, 'Maritime Museum Rotterdam - Oude Haven', NULL, 14, '1874-01-01', 'https://www.maritiemmuseum.nl', 'Tues to Sat: 10.00', 'Tues to Sat: 17.00', 733075),
 (56870962, 53.739194, -0.330217, 'The Deep, Aquarium', NULL, 12.15, '2002-03-23', 'https://thedeep.co.uk', '10.00', '18.00', 25211),
-(91412178, 51.90903, 4.487123, 'Erasmus Bridge', NULL, NULL, '1996-01-01', NULL, NULL, NULL, 733075),
+(91412178, 51.90903, 4.487123, 'Erasmus Bridge', NULL, 0, '1996-01-01', 'https://en.wikipedia.org/wiki/Erasmusbrug', NULL, NULL, 733075),
 (92158782, 53.739617, -0.333703, 'Dinostar', NULL, 3, '2005-01-01', 'http://dinostar.co.uk/', 'Sat and Sun: 11.00', 'Sat and Sun: 17.00', 25211);
 
 --
@@ -161,25 +161,25 @@ INSERT INTO `tb_poi` (`woeid_poi`, `latitude`, `longitude`, `name`, `capacity`, 
 --
 
 --
--- Indexes for table `tb_city`
+-- Indexes for table `tb_cities`
 --
-ALTER TABLE `tb_city`
+ALTER TABLE `tb_cities`
   ADD PRIMARY KEY (`woeid_city`),
   ADD UNIQUE KEY `tb_city_woeid_city_uindex` (`woeid_city`);
 
 --
--- Indexes for table `tb_photo`
+-- Indexes for table `tb_photos`
 --
-ALTER TABLE `tb_photo`
+ALTER TABLE `tb_photos`
   ADD PRIMARY KEY (`photo_id`),
   ADD UNIQUE KEY `tb_photo_photo_id_uindex` (`photo_id`),
   ADD KEY `tb_photo_woeid_city_uindex` (`woeid_city`) USING BTREE,
   ADD KEY `tb_photo_woeid_poi_uindex` (`woeid_poi`) USING BTREE;
 
 --
--- Indexes for table `tb_poi`
+-- Indexes for table `tb_pois`
 --
-ALTER TABLE `tb_poi`
+ALTER TABLE `tb_pois`
   ADD PRIMARY KEY (`woeid_poi`),
   ADD KEY `tb_poi_woeid_poi_uindex` (`woeid_poi`) USING BTREE,
   ADD KEY `tb_poi_woeid_city_uindex` (`woeid_city`) USING BTREE;
@@ -189,17 +189,17 @@ ALTER TABLE `tb_poi`
 --
 
 --
--- Constraints for table `tb_city`
+-- Constraints for table `tb_cities`
 --
-ALTER TABLE `tb_city`
-  ADD CONSTRAINT `tb_city_tb_photo_woeid_city_fk` FOREIGN KEY (`woeid_city`) REFERENCES `tb_photo` (`woeid_city`),
-  ADD CONSTRAINT `tb_city_tb_poi_woeid_city_fk` FOREIGN KEY (`woeid_city`) REFERENCES `tb_poi` (`woeid_city`);
+ALTER TABLE `tb_cities`
+  ADD CONSTRAINT `tb_city_tb_photo_woeid_city_fk` FOREIGN KEY (`woeid_city`) REFERENCES `tb_photos` (`woeid_city`),
+  ADD CONSTRAINT `tb_city_tb_poi_woeid_city_fk` FOREIGN KEY (`woeid_city`) REFERENCES `tb_pois` (`woeid_city`);
 
 --
--- Constraints for table `tb_poi`
+-- Constraints for table `tb_pois`
 --
-ALTER TABLE `tb_poi`
-  ADD CONSTRAINT `tb_poi_tb_photo_woeid_poi_fk` FOREIGN KEY (`woeid_poi`) REFERENCES `tb_photo` (`woeid_poi`);
+ALTER TABLE `tb_pois`
+  ADD CONSTRAINT `tb_poi_tb_photo_woeid_poi_fk` FOREIGN KEY (`woeid_poi`) REFERENCES `tb_photos` (`woeid_poi`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
